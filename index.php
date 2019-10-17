80f40c82fb495bf0bae82163830d897afbb32fd3
Jason Webb <zen.webb@gmail.com>
2019-10-16 21:13:25 -0500

    Changing link titles to aria-labels

--- index.php
+++ index.php
@@ -9,9 +9,9 @@
     <p>My areas of research include:</p>
     <ul>
       <li>Digital morphogenesis informed by research from the natural sciences</li>
-      <li><a href="/work/#code" title="Go to my creative coding projects">Creative coding</a> with JavaScript, p5.js, Processing, and more.</li>
-      <li><a href="/work/#fabrication" title="Go to my digital fabrication projects">Digital fabrication</a> using both conventional and experimental methods.</li>
-      <li><a href="/work/#electronics" title="Go to my electronics projects">Physical computing</a> with the Arduino platform and custom circuit boards.</li>
+      <li><a href="/work/#code" aria-label="Go to my creative coding projects">Creative coding</a> with JavaScript, p5.js, Processing, and more.</li>
+      <li><a href="/work/#fabrication" aria-label="Go to my digital fabrication projects">Digital fabrication</a> using both conventional and experimental methods.</li>
+      <li><a href="/work/#electronics" aria-label="Go to my electronics projects">Physical computing</a> with the Arduino platform and custom circuit boards.</li>
     </ul>
 
     <p>In essence, I am a <strong>creative technologist for hire</strong> focused on realizing creative professionals' visions using code, electronics, and digital fabrication. If you are an artist, agency, venue, or creative institution of any kind seeking a partner to help create a new project, product, or experience, <a href="/contact">send me a message!</a></p>
