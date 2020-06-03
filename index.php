79f34fe3e51978df315c9fd8447a9e4d91d9c24a
Jason Webb <zen.webb@gmail.com>
2020-06-02 19:16:00 -0500

    Adding Featured section, reducing SR noise, removing redundant landmark roles, making main tag code focusable

--- index.php
+++ index.php
@@ -1,27 +1,79 @@
 <?php get_header(); ?>
 
-<main class="home-page" id="main" role="main">
+<main class="home-page" id="main" tabindex="-1">
   <h1 class="visually-hidden">Home page</h1>
 
-  <section class="introduction page" aria-label="introduction">
-    <p>I am a chronically curious person with deep passions for creative and scientific inquiry, the natural world, and the mindful application of advanced and emerging technologies.</p>
-
-    <p>My areas of research include:</p>
+  <div class="introduction page">
+    <p>I am a chronically curious person with deep passions for creative and scientific inquiry, the natural world, and the mindful application of advanced and emerging technologies. My areas of interest include:</p>
     <ul>
       <li>Digital morphogenesis informed by research from the natural sciences</li>
-      <li><a href="/work/#code" aria-label="Go to my creative coding projects">Creative coding</a> with JavaScript, p5.js, Processing, and more.</li>
+      <li><a href="/work/#code" aria-label="Go to my creative coding projects">Creative coding</a> with JavaScript, shaders, Processing, Unity, and more.</li>
       <li><a href="/work/#fabrication" aria-label="Go to my digital fabrication projects">Digital fabrication</a> using both conventional and experimental methods.</li>
       <li><a href="/work/#electronics" aria-label="Go to my electronics projects">Physical computing</a> with the Arduino platform and custom circuit boards.</li>
     </ul>
 
-    <p>In essence, I am a <strong>creative technologist for hire</strong> focused on realizing creative professionals' visions using code, electronics, and digital fabrication. If you are an artist, agency, venue, or creative institution of any kind seeking a partner to help create a new project, product, or experience, <a href="/contact">send me a message!</a></p>
+    <p>In essence, I am a <strong>creative technologist for hire</strong> focused on realizing creative professionals' visions using code, electronics, and digital fabrication. If you are an artist, agency, venue, or creative institution of any kind seeking a partner to help create a new project, product, or experience, <a href="/contact">send me a message</a>!</p>
     <p>I am available for one-off commissions and collaborations, but currently prefer full-time opportunities.</p>
-  </section>
+    <a href="/contact" class="button is-ghost">Work with me</a>
+  </div>
+
+  <?php
+  // Retrieve the WP posts matching the categories requested
+  query_posts(array(
+    'category__and' => array(get_category_by_slug('work')->cat_ID),
+    'posts_per_page' => 1,
+    'meta_query' => array(array('key' => '_thumbnail_id'))
+  ));
+
+  if(have_posts()) {
+    while(have_posts()) {
+      the_post();
+
+      if(has_post_thumbnail()) {
+        $thumbnailID = get_post_thumbnail_id($post->ID);
+
+        $post_data = array(
+          "title" => get_the_title(),
+          "year" => get_the_date('Y'),
+          "url" => get_the_permalink(),
+          "full" => wp_get_attachment_image_src($thumbnailID, 'full')[0],
+          "image_large" => wp_get_attachment_image_src($thumbnailID, 'large')[0],
+          "image_medium" => wp_get_attachment_image_src($thumbnailID, 'medium')[0],
+          "image_small" => wp_get_attachment_image_src($thumbnailID, 'thumbnail')[0]
+        );
+
+        echo '
+          <a href="' . $post_data["url"] .'" class="featured">
+            <div class="title">
+              <h2>
+                <span>
+                  <b>' . $post_data["title"] . '</b>
+                  <small>(' . $post_data["year"] . ')</small>
+                </span>
+              </h2>
+            </div>
+
+            <img
+              srcset="' . $post_data["image_small"] . ' 296w,
+                      ' . $post_data["image_medium"] . ' 444w,
+                      ' . $post_data["full"] . ' 888w"
+              sizes="(max-width: 768px) 296px,
+                    (max-width: 1023px) 444px,
+                    888px"
+              src="' . $post_data["full"] . '"
+              alt=""
+            >
+          </a>
+        ';
+      }
+    }
+  }
+  ?>
 
   <ul class="tiles" aria-label="portfolio samples"></ul>
 
   <div class="see-more-wrapper">
-    <a href="/work" class="is-ghost">See more of my work</a>
+    <a href="/work" class="button is-ghost">See more of my work</a>
   </div>
 </main>
 
