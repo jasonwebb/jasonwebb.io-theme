68f7aebd0ef220894fee99f9bff0fcfdd54b5435
Jason Webb <zen.webb@gmail.com>
2020-06-07 09:19:18 -0500

    Pulling home page text from CMS for easier updating

--- index.php
+++ index.php
@@ -4,17 +4,11 @@
   <h1 class="visually-hidden">Home page</h1>
 
   <div class="introduction page">
-    <p>I am a chronically curious person with deep passions for creative and scientific inquiry, the natural world, and the mindful application of advanced and emerging technologies. My areas of interest include:</p>
-    <ul>
-      <li>Digital morphogenesis informed by research from the natural sciences</li>
-      <li><a href="/work/#code" aria-label="Go to my creative coding projects">Creative coding</a> with JavaScript, shaders, Processing, Unity, and more.</li>
-      <li><a href="/work/#fabrication" aria-label="Go to my digital fabrication projects">Digital fabrication</a> using both conventional and experimental methods.</li>
-      <li><a href="/work/#electronics" aria-label="Go to my electronics projects">Physical computing</a> with the Arduino platform and custom circuit boards.</li>
-    </ul>
-
-    <p>In essence, I am a <strong>creative technologist for hire</strong> focused on realizing creative professionals' visions using code, electronics, and digital fabrication. If you are an artist, agency, venue, or creative institution of any kind seeking a partner to help create a new project, product, or experience, <a href="/contact">send me a message</a>!</p>
-    <p>I am available for one-off commissions and collaborations, but currently prefer full-time opportunities.</p>
-    <a href="/contact" class="button is-ghost">Work with me</a>
+    <?php
+      $page = get_page_by_title('Home');
+      $content = apply_filters('the_content', $page->post_content);
+      echo $content;
+    ?>
   </div>
 
   <?php
