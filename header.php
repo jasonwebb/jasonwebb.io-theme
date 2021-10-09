d61b179cb2e8789250ebcc5cb13bb5643181d203
Jason Webb <zen.webb@gmail.com>
2021-10-08 20:20:04 -0500

    Preloading main font to prevent FOUC

--- header.php
+++ header.php
@@ -18,12 +18,16 @@
 	?>
   </title>
 
-  <link href="//cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css" rel="stylesheet">
-  <link href="//maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css" rel="stylesheet">
-  <link href="https://fonts.googleapis.com/css?family=Roboto%20Mono:400,500&display=swap" rel="stylesheet">
-  <link href="<?= get_template_directory_uri(); ?>/dist/styles.min.css" rel="stylesheet">
 
 	<?php wp_head(); ?>
+
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link rel="preload" as="style" href="https://fonts.googleapis.com/css?family=Roboto%20Mono:400,500&display=block">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto%20Mono:400,500&display=block">
+
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.6.1/css/bulma.min.css" rel="stylesheet">
+  <link href="<?= get_template_directory_uri(); ?>/dist/styles.min.css" rel="stylesheet">
+  <link href="https://maxcdn.icons8.com/fonts/line-awesome/1.1/css/line-awesome-font-awesome.min.css" rel="stylesheet">
 </head>
 
 <body>
