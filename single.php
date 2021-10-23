53b485da64e3710f1296ebe4578f171dfe2ec1c2
Jason Webb <zen.webb@gmail.com>
2021-10-23 11:24:50 -0500

    Simplyfing post details to just the date.

--- single.php
+++ single.php
@@ -18,30 +18,7 @@
       ?>
 
       <h1 class="title"><?php the_title(); ?></h1>
-
-      <div class="details">
-        Added on <?php the_date(); ?> in
-
-        <?php
-          $categories = get_the_category();
-
-          if(is_array($categories) && count($categories) > 0) {
-        ?>
-
-          <ul class="categories">
-
-            <?php
-              foreach($categories as $category)
-                if($category->slug != "portfolio" && $category->slug != "work")
-                  echo '<li><a href="/category/' . $category->slug . '" class="tag is-light">' . $category->name . '</a></li>';
-            ?>
-
-          </ul>
-
-        <?php
-          }
-        ?>
-      </div>
+      <div class="date"><?php the_date(); ?></div>
     </div>
 
     <div class="content">
